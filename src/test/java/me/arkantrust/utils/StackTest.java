581ce0c5b5fbe97a53066d59c736213670e59705
arkantrust <davidulce26@gmail.com>
2023-10-15 01:05:03 -0500

    Added tests for Stack

---
+++ src/test/java/me/arkantrust/utils/StackTest.java
@@ -0,0 +1,76 @@
+package me.arkantrust.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class StackTest {
+
+    private Stack<Integer> stack;
+
+    @BeforeEach
+    public void setUp() {
+
+        stack = new Stack<>();
+
+    }
+
+    @Test
+    public void testPush() {
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        assertEquals(3, stack.size());
+
+    }
+
+    @Test
+    public void testPop() {
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        assertEquals(3, stack.pop());
+        assertEquals(2, stack.pop());
+        assertEquals(1, stack.pop());
+
+    }
+
+    @Test
+    public void testPeek() {
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        assertEquals(3, stack.peek());
+        assertEquals(3, stack.peek());
+
+    }
+
+    @Test
+    public void testIsEmpty() {
+
+        Stack<Integer> stack = new Stack<>();
+
+        assertTrue(stack.isEmpty());
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        assertFalse(stack.isEmpty());
+
+        stack.pop();
+        stack.pop();
+        stack.pop();
+
+        assertTrue(stack.isEmpty());
+
+    }
+    
+}
