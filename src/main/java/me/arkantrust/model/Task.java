201abe463a6a4a3109dd70bae01a1803e9cd62eb
arkantrust <davidulce26@gmail.com>
2023-11-06 17:44:40 -0500

    Added task methods

--- src/main/java/me/arkantrust/model/Task.java
+++ src/main/java/me/arkantrust/model/Task.java
@@ -4,16 +4,67 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Task {
-    
-    private String description;
+public class Task implements Comparable<Task> {
+
+    private Integer id;
+    private String title;
+    private String details;
     private Priority priority;
-    private int duration;
-    private LocalDateTime createdAt;
+    private LocalDate dueDate;
+
+    public Task(String title, String details, Priority priority, LocalDate dueDate) {
+
+        this.title = title;
+        this.details = details;
+        this.priority = priority;
+        this.dueDate = dueDate;
+
+    }
+
+    public Task(String title, String details, LocalDate dueDate) {
+
+        this.title = title;
+        this.details = details;
+        this.priority = Priority.NONE;
+        this.dueDate = dueDate;
+
+    }
+
+    public Task(String title, String details) {
+
+        this.title = title;
+        this.details = details;
+        this.priority = Priority.NONE;
+        this.dueDate = LocalDate.now();
+
+    }
+
+    public Task(String title, String details, Priority priority) {
+
+        this.title = title;
+        this.details = details;
+        this.priority = priority;
+        this.dueDate = LocalDate.now();
+
+    }
+
+    @Override
+    public int compareTo(Task task) {
+
+        return this.priority.compareTo(task.getPriority());
+
+    }
+
+    @Override
+    public String toString() {
+
+        return this.title;
+
+    }
 
 }
